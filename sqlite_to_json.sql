2793bc7b9d296c9e7be09131b4d477eeff47c4a5
Jean Landim <jewanbb@gmail.com>
2021-12-08 20:35:02 -0300

    Adicionad sqlite_to_json.sql os valores do campos de INSERT INTO o delimitador de string

--- sqlite_to_json.sql
+++ sqlite_to_json.sql
@@ -1,18 +1,18 @@
-CREATE TABLE new_socios (cnpj_b TEXT, socios TEXT);
+CREATE TABLE new_socios (cnpj_b TEXT', socios TEXT);
 CREATE INDEX idx_new_socios_cnpj_b ON new_socios (cnpj_b);
-INSERT INTO new_socios (cnpj_b, socios) SELECT cnpj_b,
+INSERT INTO new_socios (cnpj_b', socios) SELECT cnpj_b',
        "[" || Group_concat(
-       "{'cnpj': " || cnpj ||
-       ",'id_socio': "|| id_socio ||
-       ", 'nom_socio': " || nom_socio ||
-       ", 'cpf_socio': " || cpf_socio ||
-       ", 'qual_socio': " || qual_socio ||
-       ", 'dt_ent_soc': " || dt_ent_soc ||
-       ", 'pais': " || pais ||
-       ", 'rep_legal': " || rep_legal ||
-       ", 'nome_rep': " || nome_rep ||
-       ", 'qual_rep_legal': " || qual_rep_legal ||
-       ", 'faixa_etaria': " || faixa_etaria || "},") || "]"
+       "{'cnpj':'" || cnpj ||
+       "','id_socio':'"|| id_socio ||
+       "','nom_socio':'" || nom_socio ||
+       "','cpf_socio':'" || cpf_socio ||
+       "', 'qual_socio':'" || qual_socio ||
+       "', 'dt_ent_soc':'" || dt_ent_soc ||
+       "', 'pais':'" || pais ||
+       "', 'rep_legal':'" || rep_legal ||
+       "', 'nome_rep':'" || nome_rep ||
+       "', 'qual_rep_legal':'" || qual_rep_legal ||
+       "', 'faixa_etaria':'" || faixa_etaria || "'},") || "]"
 FROM   socios
 GROUP  BY cnpj_b;
 
