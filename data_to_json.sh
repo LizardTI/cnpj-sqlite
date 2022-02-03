735b34097391f3fac0fbb063354a9a953bc8d753
Jean Landim <jewanbb@gmail.com>
2022-02-03 16:22:44 -0300

    Consertado o sed do data_to_json.sh

--- data_to_json.sh
+++ data_to_json.sh
@@ -5,4 +5,4 @@ echo "Generating cnpj.json"
 sqlite3 dados-publicos/cnpj.db < sqlite_to_json.sql
 
 echo "Removing undesirable characters from cnpj.json"
-sed -i "s/\[//g; s/\]//g; s/},/}/g" cnpj.json
+sed -i "s/^\[//g; s/]$//g; s/},/}/g" cnpj.json
